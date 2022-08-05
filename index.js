2a68d3f65ebdd17ac382669b8487f0f5e7fa992b
Tharu <u7145170@anu.edu.au>
2022-08-05 19:12:25 +1000

    Add finishing comments

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const fs = require('fs/promises')
 
-
+//We just created a server that listens for http requests on a certain port. 
+// Then depending on the url we write a certain file to it
 const port = 8080
 
 // In the server we filter the resoponse we send based on the url in the req obj
@@ -11,6 +12,7 @@ const server = http.createServer(async (req, res) => {
     const file = await fs.readFile('./index.html', { encoding: 'utf8' })
     // The HEAD/meta data of the response
     res.writeHead(200,'Content-type', 'text/html');
+    console.log(req)
     res.write(file);
     res.end();
   } else if (url == '/about') {
